15c5be7284e6485a6f3bd56aedb15fb0932a37ce
gowthamReddy <gowthamreddy.6991@gmail.com>
2023-09-10 21:05:41 +0530

    report with screenShots

--- src/test/java/com/pages/LoginPage.java
+++ src/test/java/com/pages/LoginPage.java
@@ -1,56 +1,59 @@
 package com.pages;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import com.aventstack.extentreports.Status;
 import com.gowtham.utils.EventUtil;
+import com.gowtham.utils.ReporterUtil;
 
-import freemarker.log.Logger;
 import io.qameta.allure.Step;
 
 public class LoginPage extends EventUtil {
-	
+
 	private WebDriver driver;
-	
+
 	private static ThreadLocal<LoginPage> threadLocal = new ThreadLocal<>();
-	
+
 	public static void set(LoginPage loginPage) {
 		threadLocal.set(loginPage);
 	}
-	
+
 	public static LoginPage get() {
 		return threadLocal.get();
 	}
-	
-	
+
+
 	@FindBy(xpath = "//android.widget.EditText[contains(@text,'Username')]")
 	private WebElement txtUserName;
-	
+
 	@FindBy(xpath ="//android.widget.EditText[@content-desc=\"test-Password\"]")
 	private WebElement txtPassword;
-	
+
 	@FindBy(xpath="//android.view.ViewGroup[@content-desc=\"test-LOGIN\"]")
 	private WebElement btnLogin;
-	
+
 	public LoginPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 		this.driver = driver;
 	}
-		
-	
+
+
 	@Step("check weather the login page is displayed or not")
 	public boolean isLoginPageDisplayed() {
 		return checkElementExists(txtUserName);
 	}
-	
+
 	@Step("login through userName:{0} and password:{1}")
-	public void login(String userName, String passWord) {
+	public void login(String userName, String passWord) throws IOException {
 		enterValue(txtUserName,userName );
 		enterValue(txtPassword, passWord);
+		ReporterUtil.get().writeLog(Status.PASS	,"Enter Username and password","Secrets Page");
 		clickElement(btnLogin);
-
 	}
-	
+
 }
--- src/test/java/com/tests/sampleTest.java
+++ src/test/java/com/tests/sampleTest.java
@@ -1,13 +1,21 @@
 package com.tests;
 
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+import com.gowtham.utils.Driver;
 import com.gowtham.utils.GlobalVariables;
 import com.gowtham.utils.WaitUtils;
 import com.pages.Configuration;
 import com.pages.LoginPage;
+import com.gowtham.utils.ReporterUtil;
 
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
@@ -18,20 +26,29 @@ public class sampleTest extends Configuration {
 
 	@Test(priority = 1,description = "this is the firstTest in the mobileTesting I'm creating")
 	@Severity(SeverityLevel.NORMAL)
-	@Description("SomkeTest")
-	@Story("TestCase_0001")
-	public void ticket_01() {
+	@Description("sanityTest")
+	@Story("testCase_01")
+	public void TC_01() throws IOException {
 		if (LoginPage.get().isLoginPageDisplayed()) {
 			System.out.println("the login page is displayed");
+			ReporterUtil.get().writeLog(Status.PASS	,"Login page is displayed","loginPage");
 		} else {
+			ReporterUtil.get().writeLog(Status.PASS	,"Login page is not displayed","loginPage");
 			Assert.fail("Login page is not displayed on launching the application");
 		}
+		ReporterUtil.get().writeLog(Status.PASS	,"trying to enter username and password");
 		LoginPage.get().login(GlobalVariables.envData.get("username"), GlobalVariables.envData.get("password"));
+		WaitUtils.constantWait(4000);
+		ReporterUtil.get().writeLog(Status.PASS	,"landing in mainPage","mainPage");
+		byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+		attachScreenshot(screenshot);
 		WaitUtils.constantWait(4000);
 	}
 
 
-
-
+	@Attachment(value = "Screenshot", type = "image/png")
+	public byte[] attachScreenshot(byte[] screenshot) {
+		return screenshot;
+	}
 
 }
